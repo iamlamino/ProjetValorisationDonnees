227a110c3320ef59594a8f839969ab579a443a6a
iamlamino <138528196+iamlamino@users.noreply.github.com>
2023-10-23 21:53:30 +0000

    Update choice.py
    j'ai optimisé le code :
    j'ai directement importé la méthode choice de la classe random

--- PYTHON/choice.py
+++ PYTHON/choice.py
@@ -1,5 +1,5 @@
 ## Importation de la fonction random
-import random
+from random import choice
 
 # CE programme genere aleatoirement des avis en fontion du joueur et de son post 
 noms = ["Sadio Mane","Aldiouma Mbaye","GANA GUEYE","NAMPALYS MENDY","PAPE MATAR SARR", "EDOUARD MENDY", "ISMAYLA SARR", "HABIB DIALLO"]
@@ -7,9 +7,9 @@ post = ["GARDIEN", "DEFENSSEUR", "MILIEUX", "ATTAQUANT"]
 avis = ["Il serait un bon joueur de football.", "Il aura du mal à s'adapter.", "Il n'est pas fait pour ce poste:)"]
 
 # Génération aléatoire  pour mes trois listes nom pos et avis
-nom_aleatoire = random.choice(noms)
-post_aleatoire = random.choice(post)
-avis_aleatoire = random.choice(avis)
+nom_aleatoire = choice(noms)
+post_aleatoire = choice(post)
+avis_aleatoire = choice(avis)
 
 # Affichage de l'avis generer 
 print("Nom :", nom_aleatoire)
