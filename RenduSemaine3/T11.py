10926d1371b4197ad5920b53977d2c8cbe60f5ca
ebqydnjni <Aldioumambaye20@gmail.com>
2023-11-15 19:53:38 +0000

    RenduSemaine3

--- RenduSemaine3/T11.py
+++ RenduSemaine3/T11.py
@@ -8,15 +8,23 @@ conn = mysql.connector.connect(
 )
 
 cursor = conn.cursor()
+
 # Récupération des données de la table Product en utilisant une jointure avec Categories
-cursor.execute("SELECT * FROM products")
-res = cursor.fetchall()
-for i in res:
-    nameC = i[2].split('|')
-    taille=len(nameC)
-    nomCat = nameC[taille -1]
-    cursor.execute("SELECT * FROM products p, categories c WHERE p.categorieProduit LIKE '%:nomCat' AND c.nomcategorie =:nomCat  ")
-            
+cursor.execute("SELECT idProduit, nomProduit, prix, poidsExpedition, idCategories FROM products P JOIN categories c ON c.nomCategorie = TRIM(SUBSTRING_INDEX(P.categorieProduit, '|', -1))")
+result = cursor.fetchall()
+
+# Insertion des données dans la table produit
+insert_query = "INSERT INTO produit (idProduit, nomProduit, prix, poids, idCategories) VALUES (%s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE nomProduit=VALUES(nomProduit), prix=VALUES(prix), poids=VALUES(poids), idCategories=VALUES(idCategories)"
+
+for row in result:
+    # Vérification si l'idProduit existe déjà dans la table produit
+    check_query = "SELECT idProduit FROM produit WHERE idProduit = %s"
+    cursor.execute(check_query, (row[0],))
+    existing_row = cursor.fetchone()
+
+    if not existing_row:
+        # Si l'idProduit n'existe pas, alors on peut l'insérer
+        cursor.execute(insert_query, row)
+
 conn.commit()
 conn.close()
-
--- RenduSemaine3/T9.py
+++ RenduSemaine3/T9.py
@@ -13,18 +13,24 @@ cursor = conn.cursor()
 cursor.execute("SELECT idCommande FROM Commandes")
 orders = cursor.fetchall()
 
-cursor.execute("SELECT idProduit FROM Products")
+cursor.execute("SELECT idProduit FROM Produit")
 products = cursor.fetchall()
 
 for order in orders:
-    line_numbers = random.sample(range(1, 4), k=random.randint(1, 3))  # Générer 1 à 3 numéros de ligne uniques
-    
-    for line_number in line_numbers:
+    # Sélectionner les lignes existantes pour cette commande
+    cursor.execute("SELECT noLigne FROM LignesCommande WHERE idCommande = %s", (order[0],))
+    existing_line_numbers = [row[0] for row in cursor.fetchall()]
+
+    # Générer des numéros de ligne uniques pour cette commande
+    available_line_numbers = list(set(range(1, 4)) - set(existing_line_numbers))
+    line_numbers_to_generate = random.sample(available_line_numbers, k=random.randint(1, min(len(available_line_numbers), 3)))
+
+    for line_number in line_numbers_to_generate:
         product = random.choice(products)[0]
         quantity = random.randint(1, 4)
-        
-        cursor.execute("INSERT INTO LignesCommande (noLigne, quantite, idProduit) VALUES (%s, %s, %s)",
-                       (line_number, quantity, product))
+
+        cursor.execute("INSERT INTO LignesCommande (idCommande, noLigne, quantite, idProduit) VALUES (%s, %s, %s, %s)",
+                       (order[0], line_number, quantity, product))
 
 conn.commit()
 conn.close()
